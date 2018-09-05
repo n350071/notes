1172f4320120aaaf2ca082374a63ea6b8c28f0c3
nao0515ki <ishigaki0515naoki@gmail.com>
2018-09-05 15:59:21 +0900

    activerecord, view

--- Rails/ActiveRecord/activerecord.rb
+++ Rails/ActiveRecord/activerecord.rb
@@ -31,3 +31,17 @@ end
 ## difference between destroy and delete
 Model.find_by(col: "foo").destroy #=> callback will be called
 Model.find_by(col: "foo").delete  #=> callback won't be called
+
+###################################################################
+## Rails counter_cache not updating correctly
+class UserRelation < ActiveRecord::Base
+  belongs_to :user, counter_cache: :relation_count
+  belongs_to :relation
+  before_destroy {user.relation_count = user.relation_count - 1} # this decrese count
+end
+class User < ActiveRecord::Base
+  def remove_relation(relation)
+    # relations.delete relation # this doesn't call callback
+    relations.destroy relation  # this calls callback
+  end
+end
