c9bb0e1943976738564839e0b27349ba76ff4ee6
n350071 <n350071@gmail.com>
2024-07-05 12:00:01 +0900

    2024-07-05

--- zero_03/marketing/marketing_名刺_business_card/名刺_ishigaki.rb
+++ zero_03/marketing/marketing_名刺_business_card/名刺_ishigaki.rb
@@ -39,7 +39,7 @@ BusinessCard.create!(
   role: "CTO",
   name: "水野賢次",
   skills: %w[Ruby React Docker AWS, etc...],
-  email: "uemura@zeroichi-hacker.com",
+  email: "mizuno@zeroichi-hacker.com",
   url: "https://zeroichi-hacker.com",
   address: "〒810-0041 福岡県福岡市中央区大名2-6-11
     Fukuoka Growth Next",
