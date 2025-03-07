e6ddabc5465a9062f049d901591b3488f06cac8e
n350071 <n350071@gmail.com>
2025-03-07 18:22:03 +0900

    save

--- zero_03/marketing/marketing_名刺_business_card/名刺_ishigaki.rb
+++ zero_03/marketing/marketing_名刺_business_card/名刺_ishigaki.rb
@@ -26,7 +26,7 @@ BusinessCard.create!(
   role: "代表取締役",
   job: "Software Engineer",
   name: "石垣尚紀",
-  skills: %w[Ruby React Docker AWS AzureOpenAI],
+  skills: %w[Ruby React Docker AWS],
   email: "ishigaki@zeroichi-hacker.com",
   url: "https://zeroichi-hacker.com",
   address: "〒819-1626 福岡県糸島市二丈波呂683",
